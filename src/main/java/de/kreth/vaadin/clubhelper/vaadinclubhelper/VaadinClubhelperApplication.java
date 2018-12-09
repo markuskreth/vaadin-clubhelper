2f90c92b3b571b4e978640f85f255f6ef91104ef
Markus Kreth <markus.kreth@web.de>
2018-12-09 23:21:46 +0100

    Deployable application

--- src/main/java/de/kreth/vaadin/clubhelper/vaadinclubhelper/VaadinClubhelperApplication.java
+++ src/main/java/de/kreth/vaadin/clubhelper/vaadinclubhelper/VaadinClubhelperApplication.java
@@ -2,13 +2,20 @@ package de.kreth.vaadin.clubhelper.vaadinclubhelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class VaadinClubhelperApplication {
+public class VaadinClubhelperApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaadinClubhelperApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(VaadinClubhelperApplication.class);
 	}
 }
