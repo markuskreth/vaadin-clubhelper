ff40a9ebdf0d0d34c87b1ad58a41da0d4fb23cfa
Markus Kreth <markus.kreth@web.de>
2019-01-23 11:38:12 +0100

    Enabled lazy loading

--- src/main/java/de/kreth/vaadin/clubhelper/vaadinclubhelper/dao/PersonDaoImpl.java
+++ src/main/java/de/kreth/vaadin/clubhelper/vaadinclubhelper/dao/PersonDaoImpl.java
@@ -1,9 +1,12 @@
 package de.kreth.vaadin.clubhelper.vaadinclubhelper.dao;
 
+import java.util.List;
+
 import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
+import de.kreth.vaadin.clubhelper.vaadinclubhelper.data.Contact;
 import de.kreth.vaadin.clubhelper.vaadinclubhelper.data.EntityAccessor;
 import de.kreth.vaadin.clubhelper.vaadinclubhelper.data.Person;
 import de.kreth.vaadin.clubhelper.vaadinclubhelper.data.Startpass;
@@ -25,6 +28,12 @@ public class PersonDaoImpl extends AbstractDaoImpl<Person> implements PersonDao 
 		Startpass startPass = obj.getStartpass();
 		if (startPass != null && startPass.hasValidId() == false) {
 			persistIfNew(startPass);
+		}
+		List<Contact> contacts = obj.getContacts();
+		if (contacts != null) {
+			for (Contact c : contacts) {
+				persistIfNew(c);
+			}
 		}
 	}
 
