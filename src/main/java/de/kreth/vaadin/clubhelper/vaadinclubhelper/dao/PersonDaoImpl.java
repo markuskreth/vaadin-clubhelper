855856754fe6da3d52f3deedd2d106ee13338743
Markus Kreth <markus.kreth@web.de>
2019-01-23 05:12:15 +0100

    PersonDao stores SubEntities (Startpass, Contact)

--- src/main/java/de/kreth/vaadin/clubhelper/vaadinclubhelper/dao/PersonDaoImpl.java
+++ src/main/java/de/kreth/vaadin/clubhelper/vaadinclubhelper/dao/PersonDaoImpl.java
@@ -1,16 +1,46 @@
 package de.kreth.vaadin.clubhelper.vaadinclubhelper.dao;
 
+import java.util.List;
+
 import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
+import de.kreth.vaadin.clubhelper.vaadinclubhelper.data.Contact;
+import de.kreth.vaadin.clubhelper.vaadinclubhelper.data.EntityAccessor;
 import de.kreth.vaadin.clubhelper.vaadinclubhelper.data.Person;
+import de.kreth.vaadin.clubhelper.vaadinclubhelper.data.Startpass;
 
 @Repository
 public class PersonDaoImpl extends AbstractDaoImpl<Person> implements PersonDao {
 
 	public PersonDaoImpl() {
 		super(Person.class);
+	}
+
+	@Override
+	public void save(Person obj) {
+		checkSubEntities(obj);
+		super.save(obj);
+	}
+
+	public void checkSubEntities(Person obj) {
+		Startpass startPass = obj.getStartpass();
+		if (startPass != null && startPass.hasValidId() == false) {
+			persistIfNew(startPass);
+		}
+		List<Contact> contacts = obj.getContacts();
+		if (contacts != null) {
+			for (Contact c : contacts) {
+				persistIfNew(c);
+			}
+		}
+	}
+
+	public void persistIfNew(EntityAccessor c) {
+		if (c.hasValidId() == false) {
+			entityManager.persist(c);
+		}
 	}
 
 	@Override
